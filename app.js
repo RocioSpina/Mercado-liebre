fa5152014ac8d567aae3a3ab68c8edfbeafbcf19
RocioSpina <rociospina809@gmail.com>
2022-08-10 21:52:01 -0300

    segundo commit de mercado liebre

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
 
-app.get()
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
